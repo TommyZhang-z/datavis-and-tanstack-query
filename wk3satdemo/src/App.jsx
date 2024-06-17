ec8c3005e2f078d83e8e20e943e8453cdf50257b
Tommy's Macbook Pro(M3 Pro) <zwt03712@gmail.com>
2024-06-17 21:46:23 +1000

    working demo

--- wk3satdemo/src/App.jsx
+++ wk3satdemo/src/App.jsx
@@ -1,24 +1,48 @@
-import { LineChart, Line, YAxis, Tooltip, ResponsiveContainer } from "recharts";
-import { usePokemonData } from "./api/queries";
+import { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+import { usePokemonData, usePokemonsData, usePokemonList } from "./api/queries";
 
 export const BASE_URL = "http://127.0.0.1:8000";
 
-function App() {
-  const { data, isLoading, error } = usePokemonData("妙蛙种子");
+function SingleMode() {
+  const pokemons = ["妙蛙种子", "杰尼龟", "皮卡丘"];
+  const [selectedPokemon, setSelectedPokemon] = useState("杰尼龟");
+  const { data, isLoading, error } = usePokemonData(selectedPokemon);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+    <div>
+      <select
+        value={selectedPokemon}
+        onChange={(e) => setSelectedPokemon(e.target.value)}
+        className="p-2 border-2 border-slate-500 rounded-md"
+      >
+        {pokemons.map((pokemon) => (
+          <option key={pokemon} value={pokemon}>
+            {pokemon}
+          </option>
+        ))}
+      </select>
       <div className="p-4 border-2 border-slate-500 rounded-md">
         <div>
-          <h1 className="text-2xl font-bold">妙蛙种子</h1>
-          <p className=" text-gray-400">妙蛙种子的战力曲线</p>
+          <h1 className="text-2xl font-bold">{selectedPokemon}</h1>
+          <p className=" text-gray-400">{selectedPokemon}的战力曲线</p>
         </div>
         <div className="w-[680px] h-[300px]">
           <ResponsiveContainer>
-            <LineChart data={data[0]} margin={{ top: 5, left: 10, right: 10 }}>
+            <LineChart
+              data={data[0]}
+              margin={{ top: 5, left: 10, right: 10, bottom: 10 }}
+            >
               <Line
                 type="monotone"
                 strokeWidth={2}
@@ -52,6 +76,162 @@ function App() {
           </ResponsiveContainer>
         </div>
       </div>
+    </div>
+  );
+}
+
+function ComparisonMode() {
+  const [selectedPokemons, setSelectedPokemons] = useState([]);
+  const pokemons = usePokemonList();
+
+  useEffect(() => {
+    if (
+      !pokemons.isLoading &&
+      pokemons.data?.length > 0 &&
+      selectedPokemons.length === 0
+    ) {
+      setSelectedPokemons(pokemons.data.slice(0, 2));
+    }
+  }, [pokemons.isLoading, pokemons.data, selectedPokemons]);
+
+  const stats = usePokemonsData(selectedPokemons);
+
+  if (stats.isLoading || pokemons.isLoading || !stats.data)
+    return <div>Loading...</div>;
+  if (stats.isError || pokemons.isError) return <div>Error...</div>;
+
+  const [data1, data2] = stats.data;
+  const merged = data1.map((item, index) => ({
+    line1: item.strength,
+    line2: data2[index].strength,
+  }));
+
+  return (
+    <div>
+      <div className="grid grid-cols-2 mb-2 gap-2">
+        <select
+          value={selectedPokemons[0]}
+          onChange={(e) =>
+            setSelectedPokemons((prev) => {
+              const newPokemons = [...prev];
+              if (e.target.value === selectedPokemons[1]) {
+                newPokemons[1] = newPokemons[0];
+              }
+              newPokemons[0] = e.target.value;
+              return newPokemons;
+            })
+          }
+          className="p-2 border-2 border-slate-500 rounded-md"
+        >
+          {pokemons.data.map((pokemon) => (
+            <option
+              key={pokemon}
+              value={pokemon}
+              // disabled={pokemon === selectedPokemons[1]}
+            >
+              {pokemon}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedPokemons[1]}
+          onChange={(e) =>
+            setSelectedPokemons((prev) => {
+              const newPokemons = [...prev];
+              if (e.target.value === selectedPokemons[0]) {
+                newPokemons[0] = newPokemons[1];
+              }
+              newPokemons[1] = e.target.value;
+              return newPokemons;
+            })
+          }
+          className="p-2 border-2 border-slate-500 rounded-md"
+        >
+          {pokemons.data.map((pokemon) => (
+            <option
+              key={pokemon}
+              value={pokemon}
+              // disabled={pokemon === selectedPokemons[0]}
+            >
+              {pokemon}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="p-4 border-2 border-slate-500 rounded-md">
+        <div>
+          <h1 className="text-2xl font-bold">
+            {selectedPokemons[0]} vs {selectedPokemons[1]}
+          </h1>
+          <p className=" text-gray-400">
+            {selectedPokemons[0]}对比{selectedPokemons[1]}的战力曲线
+          </p>
+        </div>
+        <div className="w-[680px] h-[300px]">
+          <ResponsiveContainer>
+            <LineChart
+              data={merged}
+              margin={{ top: 5, left: 10, right: 10, bottom: 10 }}
+            >
+              <Line
+                type="monotone"
+                name={selectedPokemons[0]}
+                strokeWidth={2}
+                dataKey="line1"
+                stroke="#3581B8"
+              />
+              <Line
+                type="monotone"
+                name={selectedPokemons[1]}
+                strokeWidth={2}
+                dataKey="line2"
+                stroke="#FCB07E"
+              />
+              <Legend />
+              <YAxis domain={["auto", "auto"]} hide />
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border bg-white p-2 shadow-sm">
+                        <div className="grid grid-cols-2 gap-2">
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              {selectedPokemons[0]}
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}
+                            </span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              {selectedPokemons[1]}
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+
+                  return null;
+                }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      {/* <SingleMode /> */}
+      <ComparisonMode />
     </div>
   );
 }
--- wk3satdemo/src/api/queries.js
+++ wk3satdemo/src/api/queries.js
@@ -21,3 +21,41 @@ export const usePokemonData = (pokemonName) => {
     enabled: !!pokemonName,
   });
 };
+
+const fetchPokemonsData = async (pokemonNames) => {
+  const res = await fetch(
+    `${BASE_URL}/api/v1/stats?pokemons=${pokemonNames.join(",")}`
+  );
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+};
+
+export const usePokemonsData = (pokemonNames) => {
+  return useQuery({
+    queryKey: ["pokemons", pokemonNames],
+    queryFn: () => fetchPokemonsData(pokemonNames),
+    refetchInterval: 2000,
+    staleTime: 5000,
+    enabled: !!pokemonNames.length,
+  });
+};
+
+const fetchPokemonList = async () => {
+  const res = await fetch(`${BASE_URL}/api/v1/pokemons`);
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+};
+
+export const usePokemonList = () => {
+  return useQuery({
+    queryKey: ["pokemons"],
+    queryFn: fetchPokemonList,
+    refetchInterval: 2000,
+    staleTime: 5000,
+    enabled: true,
+  });
+};
